e9ef7e4e2a696dce69b951beac0c54eeb29241ec
Daniel Im <ji641@log-1.nyu.cluster>
2021-02-16 12:28:22 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='metaopt',
+   version='1.0',
+   description='A useful module',
+   author='Daniel Jiwoong Im',
+   author_email='ji641@nyu.edu',
+   packages=['metaopt'],  #same as name
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+)
+
